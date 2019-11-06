f090520f853ddebb53b67ffdd6a8cf6612acd7d5
Denis Mekhanikov <dmekhanikov@gmail.com>
2019-11-06 15:35:33 +0300

    load text from a file

--- src/main/java/mek/search/LoadNode.java
+++ src/main/java/mek/search/LoadNode.java
@@ -1,37 +1,31 @@
 package mek.search;
 
-import mek.search.model.Match;
 import mek.search.service.TextSearchService;
 import org.apache.ignite.Ignite;
 import org.apache.ignite.Ignition;
 
-import java.util.List;
+import java.io.BufferedReader;
+import java.io.FileReader;
 
 public class LoadNode {
-    public static void main(String[] args) {
-        Ignition.setClientMode(true);
-        try (Ignite client = Ignition.start("config/ignite.xml")) {
+    public static void main(String[] args) throws Exception {
+        if (args.length != 1) {
+            System.err.println("Usage: \n\t java LoaderNode <text file>");
+        }
 
+        String path = args[0];
+
+        Ignition.setClientMode(true);
+        try (Ignite client = Ignition.start("config/ignite.xml");
+             BufferedReader br = new BufferedReader(new FileReader(path))) {
             TextSearchService searchService =
                     client.services().serviceProxy("search-service", TextSearchService.class, false);
 
-            searchService.add("Lorem ipsum dolor sit amet");
-            searchService.add("consectetur adipiscing elit");
-            searchService.add("Pellentesque a suscipit elit");
-            searchService.add("ut congue nisi.");
-            searchService.add("Etiam ullamcorper mi neque,");
-            searchService.add("eget condimentum purus bibendum et.");
-            searchService.add("Nunc pharetra, lorem eget rhoncus tempus");
-            searchService.add("odio odio pulvinar dolor");
-            searchService.add("et viverra libero libero quis libero.");
-            searchService.add("Nam vel leo condimentum, luctus nulla nec,");
-            searchService.add("commodo augue. Quisque nec enim laoreet");
-            searchService.add("ultrices dui porta, euismod felis");
+            for (String line = br.readLine(); line != null; line = br.readLine()) {
+                if (line.isEmpty())
+                    continue;
 
-            List<Match> matches = searchService.search("et", 3);
-            for (Match match : matches) {
-                System.out.println("Match: " + match.getDocument().getContent() +
-                        "; num: " + match.getNumberOfOccurrences());
+                searchService.add(line);
             }
         }
     }
